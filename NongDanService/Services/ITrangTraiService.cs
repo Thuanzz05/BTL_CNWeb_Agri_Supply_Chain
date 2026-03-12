99ea7d572da4ea2a640c59575df46c0fb56396fb
Thuan <thuantoppo@gmail.com>
2026-03-12 08:34:43 +0700

    thêm service layer cho trang trại

--- NongDanService/Services/ITrangTraiService.cs
+++ NongDanService/Services/ITrangTraiService.cs
@@ -1,6 +1,14 @@
-﻿namespace NongDanService.Services
+﻿using NongDanService.Models.DTOs;
+
+namespace NongDanService.Services
 {
     public interface ITrangTraiService
     {
+        List<TrangTraiDTO> GetAll();
+        List<TrangTraiDTO> GetByNongDan(int maNongDan);
+        TrangTraiDTO? GetById(int id);
+        int Create(TrangTraiCreateDTO dto);
+        bool Update(int id, TrangTraiUpdateDTO dto);
+        bool Delete(int id);
     }
 }
