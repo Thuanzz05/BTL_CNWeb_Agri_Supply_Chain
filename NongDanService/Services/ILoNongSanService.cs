7309445a76cce8e25ab1fa980aa5a417e2ad8a84
Thuan <thuantoppo@gmail.com>
2026-03-13 22:09:16 +0700

    thêm service layer cho lô nông sản

--- NongDanService/Services/ILoNongSanService.cs
+++ NongDanService/Services/ILoNongSanService.cs
@@ -1,6 +1,18 @@
-﻿namespace NongDanService.Services
+﻿using NongDanService.Models.DTOs;
+
+namespace NongDanService.Services
 {
     public interface ILoNongSanService
     {
+        List<LoNongSanDTO> GetAll();
+        List<LoNongSanDTO> GetByTrangTrai(int maTrangTrai);
+        List<LoNongSanDTO> GetByNongDan(int maNongDan);
+        LoNongSanDTO? GetById(int id);
+        LoNongSanDTO? GetByQRCode(string maQR);
+        List<LoNongSanDTO> GetByTrangThai(string trangThai);
+        int Create(LoNongSanCreateDTO dto);
+        bool Update(int id, LoNongSanUpdateDTO dto);
+        bool UpdateSoLuong(int id, decimal soLuongMoi);
+        bool Delete(int id);
     }
 }
