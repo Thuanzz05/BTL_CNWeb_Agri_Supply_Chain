5dbcf4d3285dd73018777fd73f7c536b4e08af6f
Thuan <thuantoppo@gmail.com>
2026-03-13 09:39:49 +0700

    thêm service layer cho sản phẩm

--- NongDanService/Services/ISanPhamService.cs
+++ NongDanService/Services/ISanPhamService.cs
@@ -1,6 +1,14 @@
-﻿namespace NongDanService.Services
+﻿using NongDanService.Models.DTOs;
+
+namespace NongDanService.Services
 {
     public interface ISanPhamService
     {
+        List<SanPhamDTO> GetAll();
+        SanPhamDTO? GetById(int id);
+        List<SanPhamDTO> SearchByName(string tenSanPham);
+        int Create(SanPhamCreateDTO dto);
+        bool Update(int id, SanPhamUpdateDTO dto);
+        bool Delete(int id);
     }
 }
--- NongDanService/Services/SanPhamService.cs
+++ NongDanService/Services/SanPhamService.cs
@@ -1,6 +1,27 @@
-﻿namespace NongDanService.Services
+﻿using NongDanService.Data;
+using NongDanService.Models.DTOs;
+
+namespace NongDanService.Services
 {
-    public class SanPhamService
+    public class SanPhamService : ISanPhamService
     {
+        private readonly ISanPhamRepository _repo;
+
+        public SanPhamService(ISanPhamRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public List<SanPhamDTO> GetAll() => _repo.GetAll();
+
+        public SanPhamDTO? GetById(int id) => _repo.GetById(id);
+
+        public List<SanPhamDTO> SearchByName(string tenSanPham) => _repo.SearchByName(tenSanPham);
+
+        public int Create(SanPhamCreateDTO dto) => _repo.Create(dto);
+
+        public bool Update(int id, SanPhamUpdateDTO dto) => _repo.Update(id, dto);
+
+        public bool Delete(int id) => _repo.Delete(id);
     }
 }
